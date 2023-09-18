b6940a92664779f5a1212c427642cca77aa774da
axdjuraev <axdjuraev@gmail.com>
2023-09-18 19:19:49 +0500

    feat: session

---
+++ session_maker/session.py
@@ -0,0 +1,25 @@
+import redis.asyncio as redis
+from typing import Protocol, runtime_checkable
+from redis.asyncio.client import Pipeline
+from axabc.db.session_mapper import LazySession as _LazySession
+
+
+@runtime_checkable
+class ExecuteAbleQuery(Protocol):
+    async def execute(self):
+        raise NotImplementedError
+
+
+class LazySession(_LazySession, Pipeline):
+    async def execute(self, query: ExecuteAbleQuery):
+        return await query.execute()
+
+    async def commit(self):
+        return self.save()
+
+    async def rollback(self):
+        return await super().discard()
+
+    async def close(self):
+        return await self.__aexit__(None, None, None)
+
