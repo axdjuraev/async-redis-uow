3e85d45210000b39207450301f44c561eee2a051
axdjuraev <axdjuraev@gmail.com>
2023-09-19 10:20:34 +0500

    feat: updateter

--- async_redis_uow/repository/common.py
+++ async_redis_uow/repository/common.py
@@ -2,9 +2,15 @@ from typing import Generic
 from .types import TIModel, TOModel
 from .adder import AdderRepo
 from .getter import GetterRepo
+from .updateter import UpdateterRepo
 
 
-class BaseRepository(AdderRepo[TIModel, TOModel], GetterRepo[TIModel, TOModel], Generic[TIModel, TOModel]):
+class BaseRepository(
+    AdderRepo[TIModel, TOModel], 
+    GetterRepo[TIModel, TOModel], 
+    UpdateterRepo[TIModel, TOModel], 
+    Generic[TIModel, TOModel],
+):
     __abstract__ = True
 
 
---
+++ async_redis_uow/repository/updateter.py
@@ -0,0 +1,17 @@
+from typing import Generic
+from .types import TIModel, TOModel
+from .base import BaseRepoCreator
+from redis.commands.json.path import Path
+
+
+class UpdateterRepo(BaseRepoCreator[TIModel, TOModel], Generic[TIModel, TOModel]):
+    __abstract__ = True
+
+    async def update(self, obj):
+        id = self.get_obj_id(obj)
+        return self.session.json().set(
+            self.hname, 
+            Path(f'$.{id}').strPath,
+            obj.dict(),
+        )
+
