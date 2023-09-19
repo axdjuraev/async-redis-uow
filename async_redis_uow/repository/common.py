5385f45976681a94b64cd1da83274cee17042470
axdjuraev <axdjuraev@gmail.com>
2023-09-19 10:26:37 +0500

    feat: DeleterRepo

--- async_redis_uow/repository/common.py
+++ async_redis_uow/repository/common.py
@@ -3,6 +3,7 @@ from .types import TIModel, TOModel
 from .adder import AdderRepo
 from .getter import GetterRepo
 from .updateter import UpdateterRepo
+from .deleter import DeleterRepo
 
 
 class BaseRepository(
@@ -10,6 +11,7 @@ class BaseRepository(
     GetterRepo[TIModel, TOModel], 
     UpdateterRepo[TIModel, TOModel], 
     Generic[TIModel, TOModel],
+    DeleterRepo[TIModel, TOModel],
 ):
     __abstract__ = True
 
