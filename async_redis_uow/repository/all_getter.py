69518fb3923d8e38d697b0b932f8c30159a9cd51
axdjuraev <axdjuraev@gmail.com>
2023-11-13 09:34:37 +0500

    ref (BaseGetters): add extra NoneType check

--- async_redis_uow/repository/all_getter.py
+++ async_redis_uow/repository/all_getter.py
@@ -19,8 +19,8 @@ class AllGetterRepo(BaseRepoCreator[TIModel, TOModel], Generic[TIModel, TOModel]
             Path(f'{filters}').strPath,
         ).execute()  # type: ignore
 
-        while objs and len(objs) == 1 and isinstance(objs, list):
+        while objs and len(objs) == 1 and isinstance(objs[-1], list):
             objs = objs[-1]
 
-        return sorted(parse_obj_as(List[self.OSchema], objs[-1]), key=self._all_sort_key)
+        return sorted(parse_obj_as(List[self.OSchema], objs), key=self._all_sort_key)
 
