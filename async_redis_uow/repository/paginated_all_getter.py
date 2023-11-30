cd9c91693ee1e74db48bef0fb62143fc254c101d
axdjuraev <axdjuraev@gmail.com>
2023-11-30 09:48:35 +0500

    ref (PaginatedAllGetterRepo:page): add parse parameter

--- async_redis_uow/repository/paginated_all_getter.py
+++ async_redis_uow/repository/paginated_all_getter.py
@@ -6,9 +6,15 @@ from .all_getter import AllGetterRepo
 class PaginatedAllGetterRepo(AllGetterRepo[TIModel, TOModel], Generic[TIModel, TOModel]):
     __abstract__ = True
 
-    async def page(self, filters: Optional[str] = None, count: Optional[int] = None, page: Optional[int] = None) -> List[TOModel]:
-        print(f'axlog: {filters=};')
-        objs = await super().all(filters)
+    async def page(
+        self, 
+        filters: Optional[str] = None, 
+        count: Optional[int] = None, 
+        page: Optional[int] = None,
+        *,
+        parse: bool = True,
+    ) -> List[TOModel]:
+        objs = await super().all(filters, parse=parse)
 
         if count and page:
             return objs[(count or 1) * page - count:count]
